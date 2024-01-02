c008f5d73d68193f3e38d66cd892d51ca1c08c98
Prashant Kumar <prashant.s.397@outlook.com>
2024-01-02 18:29:45 +0530

    built the overlays and backdrop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/LayOut/Header";
-import MealsSummary from "./components/Meals/MealsSummary";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
